06fbeb7da9ecd99a1c771686cccdf745b1f2ce93
Luis Joel S. Caballero <luijo@outlook.com>
2021-04-05 20:07:28 -0500

    Added login skeleton to Login.js

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,8 +1,45 @@
 import React from 'react';
 
 const Login = () => {
+
+    const onChange = () => {
+
+    }
+
     return(
-        <h1>Login component is properly routed</h1>
+        <div className="form-user">
+            <div className="container-form shade-dark">
+                <h1>Login</h1>
+                <form>
+                    <div className="field-form">
+                        <label htmlfor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="field-form">
+                    <label htmlfor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className='field-form'>
+                        <input type="submit" className="btn btn-primary btn-block"
+                        value="Login" />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
