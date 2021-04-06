8b97dc0362522cbb75bf6aa1b5a54245161d9c72
Luis Joel S. Caballero <luijo@outlook.com>
2021-04-05 19:33:24 -0500

    Added react router login, create accoubnt and main routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import{ BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Work Manager</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/new-account" component={NewAccount}/>
+        <Route exact path="/projects" component={Projects}/>
+      </Switch>
+    </Router>
   );
 }
 
