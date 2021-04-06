34e4a04d334911398b43f2c4f7d588cd04e56150
Luis Joel S. Caballero <luijo@outlook.com>
2021-04-05 20:43:22 -0500

    Coded NewAccount component + tested useState hook

--- client/src/components/auth/NewAccount.js
+++ client/src/components/auth/NewAccount.js
@@ -1,8 +1,96 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const NewAccount = () => {
+    //Login state
+    const [user, saveUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirm: ''
+    });
+
+    //Get from user
+    const{name, email, password, confirm} = user;
+
+    const onChange = e => {
+        saveUser({
+            ...user,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    //To let the user login
+    const onSubmit = e => {
+        e.preventDefault();
+    }
+
     return(
-        <h1>NewAccount component is properly routed</h1>
+        <div className="form-user">
+            <div className="container-form shade-dark">
+                <h1>Get an account</h1>
+                <form
+                    onSubmit={onSubmit}
+                >
+
+                    <div className="field-form">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Name"
+                            value={name}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="field-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="field-form">
+                    <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="field-form">
+                    <label htmlFor="confirm">Confirm your password</label>
+                        <input
+                            type="password"
+                            id="confirm"
+                            name="confirm"
+                            placeholder="Repeat your password"
+                            value={confirm}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className='field-form'>
+                        <input type="submit" className="btn btn-primary btn-block"
+                        value="Register" />
+                    </div>
+                </form>
+                <Link to ={'/'} className="link-account">
+                    Go back to login
+                </Link>
+            </div>
+        </div>
     );
 }
 
