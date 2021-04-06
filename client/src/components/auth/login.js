12be2eedfb49385716e8f10b5187cde2d2f86d49
Luis Joel S. Caballero <luijo@outlook.com>
2021-04-05 20:30:17 -0500

    Added the link to create a new account at Login  screen

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
     //Login state
@@ -58,6 +59,9 @@ const Login = () => {
                         value="Login" />
                     </div>
                 </form>
+                <Link to ={'/new-account'} className="link-account">
+                    Get an account
+                </Link>
             </div>
         </div>
     );
